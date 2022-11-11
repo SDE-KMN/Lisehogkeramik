a0e406d7ec6adfb90e12f9ee65fca0d12159aa6f
Kasper Høg <kasp416h@edu.sde.dk>
2022-11-11 14:22:17 +0100

    icon, media, slide, ip

--- src/Bund.jsx
+++ src/Bund.jsx
@@ -1,29 +1,24 @@
 import React, { Component } from 'react';
 import './index.css';
+import styles from './index.css';
 
 class Bund extends React.Component {
     render() {
         return (
             <div class="container-bund">
-                <h3>Kontaktoplysninger: </h3>
+                <h1>Kontaktoplysninger: </h1>
                 <table>
                     <tr>
-                        <th>Nummer</th>
-                        <td>1</td>
-                        <td>2</td>
-                        <td>3</td>
+                        <th>Nummer:</th>
+                        <td>61417696</td>
                     </tr>
                     <tr>
-                        <th>Adresse</th>
-                        <td>4</td>
-                        <td>5</td>
-                        <td>6</td>
+                        <th>Adresse:</th>
+                        <td>Adressevej 1</td>
                     </tr>
                     <tr>
-                        <th>CVR</th>
-                        <td>7</td>
-                        <td>8</td>
-                        <td>9</td>
+                        <th>E-mail</th>
+                        <td></td>
                     </tr>
                 </table>
             </div>
