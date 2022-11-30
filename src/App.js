9e4cd8565849802fd6f02f02418b805494ce94f1
Kasper Høg <kasp416h@edu.sde.dk>
2022-11-30 10:32:24 +0100

    Alt basically

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import axios from 'axios';
 
 function App() {
+  const [backendBilleder, setBackendBilleder] = useState([{}]);
+
+  useEffect(() => {
+    fetch("/billeder")
+      .then((response) => response.json())
+      .then((billeder) => {
+        setBackendBilleder(billeder);
+      });
+  }, []);
+
+  const handlepost = () => {
+    const dict = {
+      nima: "EH",
+      kasper: "TOP G CHAD",
+      mathias: 109
+    }
+    axios.post("http://localhost:5000/unlink", dict)
+    }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>
+        <button onClick={handlepost}>Hej</button>
+      </div>
+      {typeof backendBilleder.billeder === "undefined" ? (
+        <p>Loading...</p>
+      ) : (
+        backendBilleder.billeder.map((billede, i) => <p key={i}>{billede}</p>)
+      )}
     </div>
   );
 }
